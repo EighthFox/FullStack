c102b0aa8193bdce81ef0e518bbbe78c78d73be3
FPEmiliano <emivfoglino@gmail.com>
2023-06-01 12:04:47 -0300

    06change2

--- FT-M1-master/06-EstructuraDeDatos-III/homework/demo.js
+++ FT-M1-master/06-EstructuraDeDatos-III/homework/demo.js
@@ -38,6 +38,95 @@ BinarySearchTree.prototype.size = function(){
     return i;
 }
 
+BinarySearchTree.prototype.contains = function(value){
+    // var result = false;
+    // if(value === this.value){
+    //     return true
+    // }else{
+    //     if(this.left){
+    //         result = result || (this.left.contains(value))
+    //     }
+    //     else{
+    //         return false;
+    //     }
+    //     if(this.right){
+    //         result = result || (this.right.contains(value))
+    //     }
+    //     else{
+    //         return false
+    //     }
+    //     return result;
+    // }
+    var result = false;
+    if(value === this.value){
+        return true
+    }
+    if(this.left){
+        result = result || (this.left.contains(value))
+    }
+    if(this.right){
+        result = result || (this.right.contains(value))
+    }
+    return result;
+}
+
+BinarySearchTree.prototype.depthFirstForEach = function (order){
+    var tree = [];
+    switch (order){
+        case "post-order":
+            if(this.left){
+                tree = tree.concat(this.left.depthFirstForEach(order));
+            }
+            if(this.right){
+                tree = tree.concat(this.right.depthFirstForEach(order));
+            }
+            tree.push(this.value);
+            break;
+        case 'pre-order':
+            tree.push(this.value);
+            if(this.left){
+                tree = tree.concat(this.left.depthFirstForEach(order))
+            }
+            if(this.right){
+                tree = tree.concat(this.right.depthFirstForEach(order))
+            }
+            break;
+        case 'in-order':
+        default:
+            if(this.left){
+                tree = tree.concat(this.left.depthFirstForEach(order))
+            }
+            tree.push(this.value);
+            if(this.right){
+                tree = tree.concat(this.right.depthFirstForEach(order))
+            }
+            break;
+    }
+    return tree;
+}
+
+BinarySearchTree.prototype.breadthFirstForEach = function (cb, pend){
+    // var tree = [];
+    // tree.push(this.value);
+    // if(this.left && this.right){
+    //     tree.push(this.left.value);
+    //     tree.push(this.right.value);
+    //     //tree = tree.concat(this.left.breadthFirstForEach())
+    //     //tree = tree.concat(this.right.breadthFirstForEach())
+    // }
+    // return tree;
+    if (!pend){
+        var pend = [];
+    }
+    cb(this.value);
+
+    if (this.left) pend.push(this.left);
+    if (this.right) pend.push(this.right);
+
+    if (pend > 0 ) pend.shift().breadthFirstForEach(cb, pend);
+}
+
+
 const root = new BinarySearchTree(8);
 console.log(root)
 root.insert(6)
@@ -49,3 +138,15 @@ console.log(root)
 root.insert(7)
 console.log(root)
 console.log(root.size())
+console.log(root.contains(5))
+console.log(root.depthFirstForEach('post-order'))
+
+var arry = []
+arry = arry.concat([4])
+console.log(arry)
+arry = arry.concat([7])
+console.log(arry)
+arry.push(6)
+console.log(arry)
+arry.push(10)
+console.log(arry)
