959d16ba7fc8ed1aafb14e19975aa8a5da31866c
Emiliano Foglino <emivfoglino@gmail.com>
2023-06-08 18:06:52 -0300

    03initial3

--- FT-M2-master/03-ES6/homework/default.js
+++ FT-M2-master/03-ES6/homework/default.js
@@ -16,3 +16,9 @@ const miObj2 = { ...miObj}
 miObj.c = 'c';
 
 console.log(miObj)
+
+function f(x, y, z) {
+    return x + y + z;
+}
+
+f(...[1, 2, 3]); //=> para el array separado y por lo tanto cada número es un parámetro
