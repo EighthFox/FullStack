75f4c1a738578ffca9315bc0ab850e3a302a56e5
Emiliano Foglino <emivfoglino@gmail.com>
2023-06-09 19:29:59 -0300

    03almostready

--- FT-M2-master/03-ES6/homework/src/selector.js
+++ FT-M2-master/03-ES6/homework/src/selector.js
@@ -9,6 +9,12 @@ var traverseDomAndCollectElements = function (matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
+
+  const allElements = startEl.getElementsByTagName('*');
+  for(element of allElements){
+    if(matchFunc(element)) resultSet.push(element)
+  }
+  return resultSet;
 };
 
 // Detecta y devuelve el tipo de selector
@@ -16,6 +22,16 @@ var traverseDomAndCollectElements = function (matchFunc, startEl) {
 
 var selectorTypeMatcher = function (selector) {
   // tu código aquí
+  if(selector.at(0) === '#'){
+    return 'id'
+  }
+  if(selector.at(0) === '.'){
+    return 'class'
+  }
+  if(selector.includes('.') && selector.at(0) != '.'){
+    return 'tag.class'
+  }
+  return 'tag'
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -27,9 +43,32 @@ var matchFunctionMaker = function (selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") {
+    matchFunction = arg => {
+      if(arg.id === selector.slice(1)) return true;
+      return false
+    }
   } else if (selectorType === "class") {
+    matchFunction = arg => {
+      const arrSelector = arg.className.split(' ');
+      for(let i = 0; i < arrSelector.length; i++){
+        if(arrSelector[i] === selector.slice(1)) return true;
+      }
+      return false
+    }
   } else if (selectorType === "tag.class") {
+    matchFunction = arg => {
+      const classSelector = selector.split('.');
+      const arrSelector = arg.className.split(' ');
+      for(let i = 0; i < arrSelector.length; i++){
+        if(arrSelector[i] === classSelector[1] && arg.tagName.toLowerCase() === classSelector[0]) return true;
+      }
+      return false
+    }
   } else if (selectorType === "tag") {
+    matchFunction = arg => {
+      if(arg.tagName.toLowerCase() === selector) return true;
+      return false;
+    }
   }
   return matchFunction;
 };
