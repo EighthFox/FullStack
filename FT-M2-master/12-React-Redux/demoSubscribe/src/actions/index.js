65673a9e775a0d0f6b3407c80210b879b4e6351b
Emiliano Foglino <emivfoglino@gmail.com>
2023-06-25 17:46:50 -0300

    11Redux01

--- FT-M2-master/12-React-Redux/demoSubscribe/src/actions/index.js
+++ FT-M2-master/12-React-Redux/demoSubscribe/src/actions/index.js
@@ -13,3 +13,13 @@ export function reset() {
     type: 'RESET',
   }
 }
+
+//Las actions creators que van a estar involucradas de promesas, de consultas de datos, etc. Van a estar esta sintaxis.
+export const getUser = () => {
+  //retornamos una función que recibe la capacidad de hacer dispatch. La función va a ser fetch, es decir la petición de los datos. Una vez recibo la respuesta y transformo el json que me viene en un objeto de JavaScript. Luego eso que converti en objeto que ahora se llama data, lo paso como información por dispatch.
+  return function(dispatch){
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response=>response.json())
+    .then((data) => dispatch({ type: GET_USER, payload: data }))
+  }
+}
