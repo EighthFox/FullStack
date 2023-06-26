d15b44681e994ca192cbadb6cabc2c10f56f2449
FPEmiliano <emivfoglino@gmail.com>
2023-06-26 12:14:51 -0300

    10ReactFormDemo

--- FT-M2-master/10-React-Forms/demo-forms/src/App.js
+++ FT-M2-master/10-React-Forms/demo-forms/src/App.js
@@ -1,11 +1,75 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  //const [name, setName] = useState("");
+  // const [mail, setMail] = useState("");
+  // const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [form, setForm] = useState({
+    mail: "",
+    password: "",
+  })
+
+  const mailChangeHandler = (event) => {
+    const mailValue = event.target.value;
+
+    if(!/\S+@\S+\.\S+/.test(mailValue)){
+      setError("Mail inválido");
+    }else{
+      setError("");
+    }
+
+    setForm({...form, mail: mailValue});
+  };
+
+  const passwordChangeHandler = (event) => {
+    setForm({...form, password: event.target.value});
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefaul();
+  };
+
+  // const mailChangeHandler = (event) => {
+  //   const mailValue = event.target.value;
+
+  //   if(!/\S+@\S+\.\S+/.test(mailValue)){
+  //     setError("Mail inválido");
+  //   }else{
+  //     setError("");
+  //   }
+
+  //   setMail(mailValue);
+  // };
+
+  // const nameChangeHandler = (event) => {
+  //   setName(event.target.value);
+  // };
+
+  // const passwordChangeHandler = (event) => {
+  //   setPassword(event.target.value);
+  // };
+
   return (
     <>
-      <form>
+      <form className={error && "error"} onSubmit={submitHandler}>
         <h2>Soy el Form</h2>
+        {/* <div>
+          <label>Nombre:</label>
+          <input type="text" onChange={nameChangeHandler} value={name}/>
+        </div> */}
+        <div>
+          <label>Mail:</label>
+          <input type="text" onChange={mailChangeHandler} value={form.mail}/>
+        </div>
+        <div>
+          <label>Contraseña:</label>
+          <input type="password" onChange={passwordChangeHandler} value={form.password} autoComplete="on"/>
+        </div>
+        <button type="submit" disabled={!form.mail || !form.password || error}>ENVIAR</button>
       </form>
     </>
   );
