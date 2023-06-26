d6a5ce50351cb25d67efb20fe1c1d096ec8d8031
Emiliano Foglino <emivfoglino@gmail.com>
2023-06-25 21:34:21 -0300

    FormDemo

---
+++ FT-M2-master/10-React-Forms/demo-forms/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <form>
+        <h2>Soy el Form</h2>
+      </form>
+    </>
+  );
+}
+
+export default App;
