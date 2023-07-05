b3733069107fc54240bce9750a0c332bffe0124d
FPEmiliano <emivfoglino@gmail.com>
2023-07-05 12:52:58 -0300

    06AsyncHomeworkReady

--- FT-M3-master/06-AsyncAwait/homework/01 - Exercises/exercise-two.js
+++ FT-M3-master/06-AsyncAwait/homework/01 - Exercises/exercise-two.js
@@ -21,15 +21,34 @@ args.forEach(function (arg) {
 
 async function problemA() {
   // callback version
-  exerciseUtils.readFile("poem-one/stanza-01.txt", function (err, stanza) {
-    exerciseUtils.blue(stanza);
-  });
-  exerciseUtils.readFile("poem-one/stanza-02.txt", function (err, stanza) {
-    exerciseUtils.blue(stanza);
-  });
+  // exerciseUtils.readFile("poem-one/stanza-01.txt", function (err, stanza) {
+  //   exerciseUtils.blue(stanza);
+  // });
+  // exerciseUtils.readFile("poem-one/stanza-02.txt", function (err, stanza) {
+  //   exerciseUtils.blue(stanza);
+  // });
 
   // async await version
   // Tu código acá:
+  // try{
+  //   await exerciseUtils.promisifiedReadFile("poem-two/stanza-01.txt").then(
+  //     (stanza) => exerciseUtils.blue(stanza)
+  //   ),
+  //   await exerciseUtils.promisifiedReadFile("poem-two/stanza-02.txt").then(
+  //     (stanza2) => exerciseUtils.blue(stanza2)
+  //   )
+  //   console.log("done")
+  // } catch (err) {
+
+  // }
+
+  
+
+  const promises = [exerciseUtils.promisifiedReadFile("poem-two/stanza-01.txt"), exerciseUtils.promisifiedReadFile("poem-two/stanza-02.txt")];
+  // const stanzas = await Promise.all(promises);
+  // for(const stanza of stanzas) exerciseUtils.blue(stanza)
+
+  await Promise.all(promises).then((stanzas) => { stanzas.forEach((stanza) => exerciseUtils.blue(stanza)) } )
 }
 
 async function problemB() {
@@ -38,14 +57,29 @@ async function problemB() {
   });
 
   // callback version
-  filenames.forEach((filename) => {
-    exerciseUtils.readFile(filename, function (err, stanza) {
-      exerciseUtils.blue(stanza);
-    });
-  });
+  // filenames.forEach((filename) => {
+  //   exerciseUtils.readFile(filename, function (err, stanza) {
+  //     exerciseUtils.blue(stanza);
+  //   });
+  // });
 
   // async await version
   // Tu código acá:
+  // try{
+  //   filenames.map( async (filename) => {
+  //     await exerciseUtils.promisifiedReadFile(filename).then(
+  //       (stanza) => exerciseUtils.blue(stanza)
+  //     )
+  //   })
+  //   console.log("done")
+  // } catch (err) {
+
+  // }
+
+  const promises = filenames.map(file => { return exerciseUtils.promisifiedReadFile(file) });
+  const stanzas = await Promise.all(promises);
+  // for(const stanza of stanzas) exerciseUtils.blue(stanza)
+  stanzas.forEach((stanza) => exerciseUtils.blue(stanza))
 }
 
 async function problemC() {
@@ -54,14 +88,28 @@ async function problemC() {
   });
 
   // callback version
-  filenames.forEach((filename) => {
-    exerciseUtils.readFile(filename, function (err, stanza) {
-      exerciseUtils.blue(stanza);
-    });
-  });
+  // filenames.forEach((filename) => {
+  //   exerciseUtils.readFile(filename, function (err, stanza) {
+  //     exerciseUtils.blue(stanza);
+  //   });
+  // });
 
   // async await version
   // Tu código acá:
+  try{
+    // filenames.map( async (filename) => {
+    //   await exerciseUtils.promisifiedReadFile(filename).then(
+    //     (stanza) => exerciseUtils.blue(stanza)
+    //   )
+    // })
+    for(const file of filenames){
+      const stanza = await exerciseUtils.promisifiedReadFile(file);
+      exerciseUtils.blue(stanza);
+    }
+    console.log("done")
+  } catch (err) {
+
+  } 
 }
 
 async function problemD() {
@@ -72,13 +120,28 @@ async function problemD() {
   filenames[randIdx] = "wrong-file-name-" + (randIdx + 1) + ".txt";
 
   // callback version
-  filenames.forEach((filename) => {
-    exerciseUtils.readFile(filename, function (err, stanza) {
-      exerciseUtils.blue(stanza);
-      if (err) exerciseUtils.magenta(new Error(err));
-    });
-  });
+  // filenames.forEach((filename) => {
+  //   exerciseUtils.readFile(filename, function (err, stanza) {
+  //     exerciseUtils.blue(stanza);
+  //     if (err) exerciseUtils.magenta(new Error(err));
+  //   });
+  // });
 
   // async await version
   // Tu código acá:
+  try{
+    // filenames.map( async (filename) => {
+    //   await exerciseUtils.promisifiedReadFile(filename).then(
+    //     (stanza) => exerciseUtils.blue(stanza),
+    //   )
+    // })
+    for(const file of filenames){
+      const stanza = await exerciseUtils.promisifiedReadFile(file);
+      exerciseUtils.blue(stanza);
+    }
+  } catch (err) {
+    exerciseUtils.magenta(err)
+  } finally {
+    console.log("done")
+  }
 }
