f4f9b601e42f8adea52a56b736903feae7ca0439
Emiliano Foglino <emivfoglino@gmail.com>
2023-07-07 18:40:45 -0300

    02SQLHomeWorkReady

--- FT-M4-master/02-sql/homework/01 - Exercises/homework.js
+++ FT-M4-master/02-sql/homework/01 - Exercises/homework.js
@@ -2,15 +2,18 @@
 
 const ejercicio02 = 'SELECT * FROM movies WHERE duration < 90';
 
-const ejercicio03 = 'SELECT * FROM movies WHERE year >= 1930 AND year <= 1940';
+const ejercicio03 = 'SELECT * FROM movies WHERE year >= 1930 AND year <= 1940'; //or WHERE year BETWEEN 1930 AND 1940
 
 const ejercicio04 = 'SELECT * FROM movies WHERE title LIKE '%til%'';
 
 const ejercicio05 = 'SELECT *, ARRAY_LENGTH(actors, 1) FROM movies WHERE ARRAY_LENGTH(actors, 1) = 1';
+//'SELECT * FROM movies WHERE ARRAY_LENGTH(actors, 1) = 1'
+//'SELECT * FROM movies WHERE cardinality(actors) = 1'
 
-const ejercicio06 = 'SELECT title, (SELECT AVG(r) FROM UNNEST(ratings) as r) FROM movies';
+const ejercicio06 = 'SELECT title, (SELECT AVG(r) FROM UNNEST(ratings) as r) as rating FROM movies';
+//'SELECT title, AVG(rating) FROM movies, UNNEST(ratings) rating GROUP BY title'
 
-const ejercicio07 = 'SELECT actors FROM movies WHERE title LIKE '%Fast and%' AND year = 2016';
+const ejercicio07 = 'SELECT actors FROM movies WHERE (title LIKE '%Fast and%' AND year = 2016)';
 
 module.exports = {
    ejercicio02,
